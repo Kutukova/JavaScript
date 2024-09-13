dde977f865127732221dc5386ff3ed3e22a6dcd5
Kutukova <andriienko.mariia@gmail.com>
2024-09-13 14:39:49 +0300

    HW2

---
+++ 02/HW02/math.js
@@ -0,0 +1,25 @@
+let result1 = (5 + 3) * 2 - 7;
+
+let result2 = (10 / 2) ** 2;
+
+let result3 = 2 * (4 + 3) - (8 / 2);
+
+let result4 = (15 % 4) + 5;
+
+let result5 = 3 - (2 ** 4);
+
+console.log(`result1 (5 + 3) * 2 - 7 = ${result1}`);
+console.log(`result2 (10 / 2) ** 2 = ${result2}`);
+console.log(`result3 2 * (4 + 3) - (8 / 2) = ${result3}`);
+console.log(`result4 (15 % 4) + 5 = ${result4}`);
+console.log(`result5 3 - (2 ** 4) = ${result5}`);
+
+let firstNumber = 21;
+let secondNumber = 13;
+
+console.log(`${firstNumber-secondNumber}`);
+console.log(`${firstNumber+secondNumber}`);
+console.log(`${firstNumber/secondNumber}`);
+console.log(`${firstNumber*secondNumber}`);
+console.log(`${firstNumber**2}`);
+console.log(`${firstNumber%5}`);
---
+++ 02/main.js
@@ -0,0 +1,28 @@
+let userName = 'string value';
+
+// Number
+let userAge = 10;
+let productPrice = 1.5;
+let saleValue = 0.345;
+// NaN
+// 'String'/2 -> Nan
+// Infinity, -Infinity;
+// 1/0 ->Infinity;
+
+// Boolean
+let isAdmin = true;
+let isLoginUser = false;
+// Null
+let noValue = null;
+//Undefined 
+let noFindValue = undefined;
+
+console.log('Hello from js');
+console.log(userName);
+console.log(productPrice);
+console.log(isAdmin);
+
+console.log('Is user admin:', isAdmin)
+// let result = userName+' '+userName;
+let result = `Hello template literal ${userName} has saleValue ${saleValue} more info about user ${isLoginUser} math operation 2+2=${2+2}`;
+console.log('Result', result);
